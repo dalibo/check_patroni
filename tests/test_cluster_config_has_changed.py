7256c1894a86be2809b75d783ce9549bc699fe2a
benoit <benoit.lobreau@dalibo.com>
2023-03-12 19:57:52 +0100

    Fix tests for the urllib3 to requests change

--- tests/test_cluster_config_has_changed.py
+++ tests/test_cluster_config_has_changed.py
@@ -42,7 +42,7 @@ def test_cluster_config_has_changed_ok_with_hash(mocker: MockerFixture) -> None:
             "https://10.20.199.3:8008",
             "cluster_config_has_changed",
             "--hash",
-            "640df9f0211c791723f18fc3ed9dbb95",
+            "96b12d82571473d13e890b893734e731",
         ],
     )
     print(result.output)
@@ -53,7 +53,7 @@ def test_cluster_config_has_changed_ok_with_state_file(mocker: MockerFixture) ->
     runner = CliRunner()
 
     with open(here / "cluster_config_has_changed.state_file", "w") as f:
-        f.write('{"hash": "640df9f0211c791723f18fc3ed9dbb95"}')
+        f.write('{"hash": "96b12d82571473d13e890b893734e731"}')
 
     my_mock(mocker, "cluster_config_has_changed", 200)
     result = runner.invoke(
@@ -80,7 +80,7 @@ def test_cluster_config_has_changed_ko_with_hash(mocker: MockerFixture) -> None:
             "https://10.20.199.3:8008",
             "cluster_config_has_changed",
             "--hash",
-            "640df9f0211c791723f18fc3edffffff",
+            "96b12d82571473d13e890b8937ffffff",
         ],
     )
     assert result.exit_code == 2
@@ -92,7 +92,7 @@ def test_cluster_config_has_changed_ko_with_state_file_and_save(
     runner = CliRunner()
 
     with open(here / "cluster_config_has_changed.state_file", "w") as f:
-        f.write('{"hash": "640df9f0211c791723f18fc3edffffff"}')
+        f.write('{"hash": "96b12d82571473d13e890b8937ffffff"}')
 
     my_mock(mocker, "cluster_config_has_changed", 200)
     # test without saving the new hash
@@ -113,7 +113,7 @@ def test_cluster_config_has_changed_ko_with_state_file_and_save(
     new_config_hash = cookie.get("hash")
     cookie.close()
 
-    assert new_config_hash == "640df9f0211c791723f18fc3edffffff"
+    assert new_config_hash == "96b12d82571473d13e890b8937ffffff"
 
     # test when we save the hash
     result = runner.invoke(
@@ -134,4 +134,4 @@ def test_cluster_config_has_changed_ko_with_state_file_and_save(
     new_config_hash = cookie.get("hash")
     cookie.close()
 
-    assert new_config_hash == "640df9f0211c791723f18fc3ed9dbb95"
+    assert new_config_hash == "96b12d82571473d13e890b893734e731"
