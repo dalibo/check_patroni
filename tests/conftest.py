123c300911a9fe7b78b226e9e3ae98530736402a
Denis Laxalde <denis.laxalde@dalibo.com>
2023-09-28 09:37:44 +0200

    Add type hints in tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
-def pytest_addoption(parser):
+from typing import Any
+
+
+def pytest_addoption(parser: Any) -> None:
     """
     Add CLI options to `pytest` to pass those options to the test cases.
     These options are used in `pytest_generate_tests`.
@@ -6,7 +9,7 @@ def pytest_addoption(parser):
     parser.addoption("--use-old-replica-state", action="store_true", default=False)
 
 
-def pytest_generate_tests(metafunc):
+def pytest_generate_tests(metafunc: Any) -> None:
     metafunc.parametrize(
         "use_old_replica_state", [metafunc.config.getoption("use_old_replica_state")]
     )
